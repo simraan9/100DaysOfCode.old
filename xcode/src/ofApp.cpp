2e972950745e224532ed5abb482e82e576b36499
Apoorva A <apoorva.a1998@gmail.com>
2019-01-31 11:04:14 +0530

    new changes

--- xcode/src/ofApp.cpp
+++ xcode/src/ofApp.cpp
@@ -1,6 +1,7 @@
 #include "ofApp.h"
 #include "ofMain.h"
 //--------------------------------------------------------------
+int death_count=-1;
 void ofApp::setup(){
     dummy=Agent();
     meal=Food();
@@ -11,12 +12,13 @@ void ofApp::setup(){
     dummy.setFood(meal);
     dummy.setTimer(t);
     dummy.reSpawn(meal);
-}
+    meal.checkEnergy(dummy);
+    }
 
 //--------------------------------------------------------------
 void ofApp::update(){
     if(dummy.see()>=0){
-        dummy.move();  
+        dummy.move();
     }
     t.keyPressed();
 }
@@ -29,7 +31,7 @@ void ofApp::draw(){
     //fill(255,255,255);
     meal.display();
     dummy.die();
-    meal.die();
+    meal.checkEnergy();
 }
 
 //--------------------------------------------------------------
@@ -41,10 +43,13 @@ void Food::reset(){
     this->r = ofRandom(50,350);
 }
 //--------------------------------------------------------------
-//void Food :: die(){
-  //  if (this->energy<0){
-    //   this->reset();
-    //}
+void Food :: checkEnergy(Agent energy){
+    this->energy=energy;
+    if (this->energy<0){
+        this->reset();
+    }
+}
+    
 //--------------------------------------------------------------
 void Agent::setFood(Food food){
         this->look_for = food;
@@ -52,7 +57,7 @@ void Agent::setFood(Food food){
 //--------------------------------------------------------------
 //void Agent::setTimer(Time timer){
   //  this->timer = timer;
-}
+
 //--------------------------------------------------------------
 void Agent:: reSpawn(Food doof){
     this->change_f=doof;
@@ -96,6 +101,102 @@ void Agent::  move(){
     this->energy=this->energy-1;
 }
 
+//--------------------------------------------------------------
+void Agent::flip(){
+    if (this->direction==0){
+        this->direction=1;
+    }
+    else    if (this->direction==1){
+        this->direction=0;
+    }
+}
+//--------------------------------------------------------------
+int Agent::see(){
+    if ((this->check_jumps(this->x,this->x+50)!=0) && this->direction ==1){//checks for 50px awareness in +ve direction
+        if ((this->check_jumps(this->x,(this->x+(this->energy/100)))!=0) && this->direction ==1){ //checks for speed awareness +ve direction
+            if(this->energy<=900){
+                this->eat_food();
+            }
+        }
+            else{
+                this->move();
+            }
+    }
+    else if ((this->check_jumps(this->x-50,this->x)!=0)&& this->direction ==0){ //checks for 50px awareness in -ve direction
+        if ((this->check_jumps(this->x-(this->energy/100),this->x)!=0)&& this->direction ==0){ //checks for speed awareness -ve direction
+            if(this->energy<=900){
+                this->eat_food();
+            }
+        }
+            else{
+                this->move();
+            }
+    }
+    else
+        this->move();
+    return this->flag;
+}
+//--------------------------------------------------------------
+//checks if the food is between two points
+int Agent::check_jumps(int m, int n){
+    int r = this->look_for.r;
+    if(this->direction==1){
+        for(int i=m; i<n+1; i++)
+            if(i>=r-1 && i<=r+1)
+                this->flag=i;
+                }
+    
+    else if (this->direction==0){
+        for(int j=n; j>m-1; j--)
+            if(j>=r-1 && j<=r+1)
+                this->flag=j;
+                }
+    return this->flag;
+}
+//--------------------------------------------------------------
+void Agent::eat_food(){
+    int r = this->look_for.r;
+    if(this->x>=r-(this->energy/100) && this->x<=r+(this->energy/100)){
+        this->timer.count_cycletime();
+        this->energy=this->energy+50;
+        this->change_f.reset();
+        this->flag=0;
+    }
+}
+//--------------------------------------------------------------
+void Agent:: die(){
+    if (this->energy<0){
+        this->reset();
+    }
+    this->flag=0;
+}
+//--------------------------------------------------------------
+
+//respawns the agent
+void Agent::reset(){
+    this->x = ofRandom(400);
+    this->energy=1000;
+    death_count=death_count+1;
+}
+//--------------------------------------------------------------
+void Time::count_cycletime(){
+    int time= this->frameCounts;
+    this->cycle_time= time-this->time2;
+    this->total_time=this->total_time+this->cycle_time;
+    this->avg_time=this->total_time/this->count_cycle;
+    this->time2= time;
+    this->count_cycle=this->count_cycle+1;
+}
+//--------------------------------------------------------------
+
+/*void Time:: keyPressed(){
+    if (ofGetKeyPressed() == char(65)){
+        this->simulation_speed += 20;
+    }
+    else if(ofGetKeyPressed( == char(66)){
+        this->simulation_speed -= 20;
+    }
+}*/
 //--------------------------------------------------------------
 
 
@@ -150,6 +251,6 @@ void ofApp::gotMessage(ofMessage msg){
 }
 
 //--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){  
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
 
 }
--- xcode/src/simulation.h
+++ xcode/src/simulation.h
@@ -10,84 +10,6 @@
 
 
 #endif /* simulation_h */
-
-
-class Food{
-public:
-    int r;
-    int y;
-    Food(){
-        //this.x position of food
-        r=50;
-        y=150;
-    }
-    void display();
-    void reset(); //respawns the food
-    void die();
-};
-
-class Agent {
-public:
-    int x;
-    int y;
-    int direction;
-    int radius;
-    //energy
-    int energy;
-    int flag; // only for now
-    int max_speed;
-    //get food location
-    void look_for;
-    void timer;
-    void change_f;
-    Agent(){
-        /*
-         purpose: initialize all the variables inside the object of Agent
-         input: None
-         output: Agent object
-         */
-        // position and shape
-        x=0;
-        y=150;
-        direction=0;
-        radius=30;
-        
-        //energy
-        energy=0;
-        
-        //
-        flag=0; // only for now
-        max_speed=10;
-        
-        //get food location
-        look_for = 0;
-        timer = 0;
-        change_f = 0;
-        
-    }
-    void setFood(Food food);
-    void setTimer(Time timer);
-    void reSpawn(Food doof);
-    void display();
-    void move();
-    void flip();
-    
-    //change name to see(). This function checks if the food is within 50px and returns the place value in this.flag.Change name to place_value
-    int see();
-    
-    //checks if the food is between two points
-    int check_jumps(int m, int n);
-    
-    //If the returned place_value is at the same position as the food, it consumes it and respawns food
-    void eat_food();
-    
-    //dies the agent of the this.energy<900
-    void die();
-    
-    //respawns the agent
-    void reset();
-};
-
 class Time {
 public:
     int count_cycle;
@@ -110,4 +32,80 @@ public:
     void keyPressed();
 };
 
+class Food{
+public:
+    int r;
+    int y;
+    int energy;
+    Food(){
+        //this.x position of food
+        r=50;
+        y=150;
+    }
+    void display();
+    void reset(); //respawns the food
+    void checkEnergy(Agent energy);
+};
 
+class Agent {
+public:
+    int x;
+    int y;
+    int direction;
+    int radius;
+    //energy
+    int energy;
+    int flag; // only for now
+    int max_speed;
+    //get food location
+    Food look_for;
+    Time timer;
+    Food change_f;
+    Agent(){
+        /*
+         purpose: initialize all the variables inside the object of Agent
+         input: None
+         output: Agent object
+         */
+        // position and shape
+        x=0;
+        y=150;
+        direction=0;
+        radius=30;
+        
+        //energy
+        energy=0;
+        
+        //
+        flag=0; // only for now
+        max_speed=10;
+        
+        //get food location
+        //look_for = 0;
+        //timer = 0;
+        //change_f = 0;
+        
+    }
+    
+    void setFood(Food food);
+    void setTimer(Time timer);
+    void reSpawn(Food doof);
+    void display();
+    void move();
+    void flip();
+    
+    //change name to see(). This function checks if the food is within 50px and returns the place value in this.flag.Change name to place_value
+    int see();
+    
+    //checks if the food is between two points
+    int check_jumps(int m, int n);
+    
+    //If the returned place_value is at the same position as the food, it consumes it and respawns food
+    void eat_food();
+    
+    //dies the agent of the this.energy<900
+    void die();
+    
+    //respawns the agent
+    void reset();
+};
