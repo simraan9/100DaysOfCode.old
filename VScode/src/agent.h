7b6fc75f3c9e55bc702d2372bcca5441fe3ffa97
Simran Singh <simran.singh9@yahoo.com>
2019-02-05 18:33:35 +0530

    Agent finds playground

--- VScode/src/agent.h
+++ VScode/src/agent.h
@@ -1,34 +1,37 @@
 #pragma once
 
 class Agent {
-	private:
-		int x;
-		int y;
-		int r;
-		float theta;
+private:
+	int x;
+	int y;
+	int radius;
+	float theta;
 
-		int x1;
-		int y1;
-		int x2;
-		int y2;
-		int x3;
-		int y3;
+	int x1;
+	int y1;
+	int x2;
+	int y2;
+	int x3;
+	int y3;
+	int direction;
 
 
-	public:
+public:
 
 	Agent() {
 		x = 100;
 		y = 100;
 		theta = 0;
-		r = 20;
+		radius = 20;
+		direction = 1;
 	}
 
-	Agent(int x, int y,float theta) {
+	Agent(int x, int y, float theta) {
 		this->x = 100;
 		this->y = 100;
 		this->theta = 0;
-		r = 20;
+		radius = 20;
+		direction = 1;
 	}
 	int get_x() {
 		return x;
@@ -37,16 +40,57 @@ class Agent {
 		return y;
 	}
 	void display() {
-		int x1 = x + (r*(cos(theta)));
-		int y1 = y + (r*(sin(theta)));
-		int x2 = x + (r*(cos(theta + 2 * (PI / 3) + (PI / 8))));
-		int y2 = y + (r*(sin(theta + 2 * (PI / 3) + (PI / 8))));
-		int x3 = x + (r*(cos(theta + (-4)*(PI / 6) - (PI / 8))));
-		int y3 = y + (r*(sin(theta + (-4)*(PI / 6) - (PI / 8))));
-		
+		int x1 = x + (radius*(cos(theta)));
+		int y1 = y + (radius*(sin(theta)));
+		int x2 = x + (radius*(cos(theta + 2 * (PI / 3) + (PI / 8))));
+		int y2 = y + (radius*(sin(theta + 2 * (PI / 3) + (PI / 8))));
+		int x3 = x + (radius*(cos(theta + (-4)*(PI / 6) - (PI / 8))));
+		int y3 = y + (radius*(sin(theta + (-4)*(PI / 6) - (PI / 8))));
+
 		ofDrawTriangle(x1, y1, x2, y2, x3, y3);
 
 	}
+	int flip() {
+		if (direction == 1) {
+			direction = 0;
+		}
+		else if (direction == 0) {
+			direction = 1;
+		}
+		return direction;
+	}
 
+	void move() {
+		if (x == 0) {
+			flip();
+		}
+		else if (x>1023){
+			flip();
+		}
+
+		if (x <= 1024 && direction == 0) {
+			x = x - 1;
+		}
+		else if (x > 0 && direction == 1) {
+			x = x + 1;
+		}
+	}
+};
+
+class Food {
+private:
+	int r;
+	int y;
+	
+public:
+
+	Food(){
+		this->r= 50;
+		this->y= 100;
+	}
+	
+	void displayFood(){
+		ofDrawCircle(this->r, this->y, 10, 10);
+	}
 
 };
--- VScode/src/ofApp.h
+++ VScode/src/ofApp.h
@@ -7,6 +7,7 @@ class ofApp : public ofBaseApp{
 
 	public:
 		Agent A;
+		Food meal;
 		void setup();
 		void update();
 		void draw();
